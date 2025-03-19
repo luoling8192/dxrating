2c504b75ec01e24f910a3526f642386360212374
RainbowBird <git@luoling.moe>
2025-03-19 14:25:13 +0800

    feat: luoxue support

--- src/b50/functions.ts
+++ src/b50/functions.ts
@@ -1,71 +1,77 @@
-import { IMusicChart } from './datasource';
+import type { IMusicChart } from './datasource'
 
-// Enum of achievements
+// Enum of achievements thresholds for maimai DX rating calculation
 enum Achievement {
-	C = 50.0,
-	B = 60.0,
-	BB = 70.0,
-	BBB = 75.0,
-	A = 80.0,
-	AA = 90.0,
-	AAA = 94.0,
-	S = 97.0,
-	Sp = 98.0,
-	SS = 99.0,
-	SSp = 99.5,
-	SSS = 100.0,
-	SSSp = 100.5
+  C = 50.0,
+  B = 60.0,
+  BB = 70.0,
+  BBB = 75.0,
+  A = 80.0,
+  AA = 90.0,
+  AAA = 94.0,
+  S = 97.0,
+  Sp = 98.0,
+  SS = 99.0,
+  SSp = 99.5,
+  SSS = 100.0,
+  SSSp = 100.5,
 }
 
-// Splash+ rating algorithm
+// Calculates rating value based on chart difficulty and achievement percentage
 // ref: https://github.com/Yuri-YuzuChaN/maimaiDX/blob/main/libraries/maimai_best_40.py#L406
 function computeRa(ds: number, achievement: number): number {
-	let baseRa: number = 22.4;
-	if (achievement < Achievement.C) {
-		baseRa = 0.0;
-	} else if (achievement < Achievement.B) {
-		baseRa = 8.0;
-	} else if (achievement < Achievement.BB) {
-		baseRa = 9.6;
-	} else if (achievement < Achievement.BBB) {
-		baseRa = 11.2;
-	} else if (achievement < Achievement.A) {
-		baseRa = 12.0;
-	} else if (achievement < Achievement.AA) {
-		baseRa = 13.6;
-	} else if (achievement < Achievement.AAA) {
-		baseRa = 15.2;
-	} else if (achievement < Achievement.S) {
-		baseRa = 16.8;
-	} else if (achievement < Achievement.Sp) {
-		baseRa = 20.0;
-	} else if (achievement < Achievement.SS) {
-		baseRa = 20.3;
-	} else if (achievement < Achievement.SSp) {
-		baseRa = 20.8;
-	} else if (achievement < Achievement.SSS) {
-		baseRa = 21.1;
-	} else if (achievement < Achievement.SSSp) {
-		baseRa = 21.6;
-	}
-	return Math.floor(ds * (Math.min(100.5, achievement) / 100) * baseRa);
+  let baseRa: number = 22.4
+  if (achievement < Achievement.C) {
+    baseRa = 0.0
+  }
+  else if (achievement < Achievement.B) {
+    baseRa = 8.0
+  }
+  else if (achievement < Achievement.BB) {
+    baseRa = 9.6
+  }
+  else if (achievement < Achievement.BBB) {
+    baseRa = 11.2
+  }
+  else if (achievement < Achievement.A) {
+    baseRa = 12.0
+  }
+  else if (achievement < Achievement.AA) {
+    baseRa = 13.6
+  }
+  else if (achievement < Achievement.AAA) {
+    baseRa = 15.2
+  }
+  else if (achievement < Achievement.S) {
+    baseRa = 16.8
+  }
+  else if (achievement < Achievement.Sp) {
+    baseRa = 20.0
+  }
+  else if (achievement < Achievement.SS) {
+    baseRa = 20.3
+  }
+  else if (achievement < Achievement.SSp) {
+    baseRa = 20.8
+  }
+  else if (achievement < Achievement.SSS) {
+    baseRa = 21.1
+  }
+  else if (achievement < Achievement.SSSp) {
+    baseRa = 21.6
+  }
+  return Math.floor(ds * (Math.min(100.5, achievement) / 100) * baseRa)
 }
 
-// Use new algorithm to update chart data
+// Updates rating values for an array of music charts using the current algorithm
 export function updateRa(charts: Array<IMusicChart>): Array<IMusicChart> {
-	let arr: Array<IMusicChart> = [];
-	charts.forEach((music: IMusicChart) => {
-		music.ra = computeRa(music.ds, music.achievements);
-		arr.push(music);
-	});
-	return arr;
+  return charts.map((music: IMusicChart) => ({
+    ...music,
+    ra: computeRa(music.ds, music.achievements),
+  }))
 }
 
-// Sum rating in chart data array
+// Calculates total rating from an array of music charts
 export function sumRa(charts: Array<IMusicChart>): number {
-	let ra: number = 0;
-	charts.forEach((music: IMusicChart) => {
-		ra += music.ra;
-	});
-	return ra;
+  return charts.reduce((total: number, music: IMusicChart) => total + music.ra, 0)
 }
--- src/b50/index.ts
+++ src/b50/index.ts
@@ -1,14 +1,20 @@
 // https://github.com/ekkusu-3800mhz/maimai-b50-simulator
 // https://github.com/ekkusu-3800mhz/maimai-b50-simulator/pull/1
 
-import { IMusicChart, IResponse } from './datasource';
-import { sumRa, updateRa } from './functions';
+import type { ILuoXueResponse, IMusicChart } from './datasource'
+import { sumRa, updateRa } from './functions'
 
-export default (data: IResponse) => {
-	const b35List: Array<IMusicChart> = updateRa(data.charts.sd);
-	const b15List: Array<IMusicChart> = updateRa(data.charts.dx);
-	// Calculate rating
-	const b35: number = sumRa(b35List);
-	const b15: number = sumRa(b15List);
-	return b35 + b15;
+export default (data: ILuoXueResponse) => {
+  // Convert standard charts to IMusicChart format and calculate top 35
+  const b35List: Array<IMusicChart> = updateRa(data.data.standard)
+
+  // Convert DX charts to IMusicChart format and calculate top 15 if available
+  const b15List: Array<IMusicChart> = data.data.dx ? updateRa(data.data.dx) : []
+
+  // Calculate rating by summing top scores
+  const b35: number = sumRa(b35List)
+  const b15: number = sumRa(b15List)
+
+  // Return total rating (B35 + B15)
+  return b35 + b15
 }
